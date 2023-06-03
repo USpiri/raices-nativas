1972c1a1197731d1a03d1cac174ab6a6bd040cae
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-03 11:40:18 -0300

    ✨ Feat: Sidewalks buttons

--- src/app/modules/sidewalk/sidewalk.module.ts
+++ src/app/modules/sidewalk/sidewalk.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidewalkComponent } from './sidewalk.component';
 import { SidewalkRoutingModule } from './sidewalk-routing.module';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [SidewalkComponent],
-  imports: [CommonModule, SidewalkRoutingModule],
+  imports: [CommonModule, SidewalkRoutingModule, MaterialModule],
 })
 export class SidewalkModule {}
