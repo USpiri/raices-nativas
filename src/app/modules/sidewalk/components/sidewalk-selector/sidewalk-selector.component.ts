38165dcc6529f2bad15642d88ead4f544f8d0ebe
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-03 12:00:01 -0300

    ✨ Feat: Selector event emitter

--- src/app/modules/sidewalk/components/sidewalk-selector/sidewalk-selector.component.ts
+++ src/app/modules/sidewalk/components/sidewalk-selector/sidewalk-selector.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidewalk-selector',
   templateUrl: './sidewalk-selector.component.html',
-  styleUrls: ['./sidewalk-selector.component.scss']
+  styleUrls: ['./sidewalk-selector.component.scss'],
 })
 export class SidewalkSelectorComponent {
-
+  @Output() selectedOption: EventEmitter<unknown> = new EventEmitter();
+  selectOption() {
+    this.selectedOption.emit();
+  }
 }
--- src/app/modules/sidewalk/sidewalk.component.ts
+++ src/app/modules/sidewalk/sidewalk.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidewalk',
   templateUrl: './sidewalk.component.html',
-  styleUrls: ['./sidewalk.component.scss']
+  styleUrls: ['./sidewalk.component.scss'],
 })
 export class SidewalkComponent {
-
+  isSelectorActive = true;
+  onSelectOption() {
+    this.isSelectorActive = false;
+  }
 }
