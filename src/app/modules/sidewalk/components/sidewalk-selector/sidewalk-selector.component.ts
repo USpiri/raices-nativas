a5f40fa86fc76be270be21e2f3e99ae75b71fdf6
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-03 11:51:50 -0300

    ✨ Feat: Sidewalk selector and trees rendering components

---
+++ src/app/modules/sidewalk/components/sidewalk-selector/sidewalk-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidewalk-selector',
+  templateUrl: './sidewalk-selector.component.html',
+  styleUrls: ['./sidewalk-selector.component.scss']
+})
+export class SidewalkSelectorComponent {
+
+}
--- src/app/modules/sidewalk/sidewalk.module.ts
+++ src/app/modules/sidewalk/sidewalk.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SidewalkComponent } from './sidewalk.component';
 import { SidewalkRoutingModule } from './sidewalk-routing.module';
 import { MaterialModule } from './shared/material.module';
+import { SidewalkSelectorComponent } from './components/sidewalk-selector/sidewalk-selector.component';
+import { TreesComponent } from './components/trees/trees.component';
+import { TreeComponent } from './components/trees/components/tree/tree.component';
 
 @NgModule({
-  declarations: [SidewalkComponent],
+  declarations: [
+    SidewalkComponent,
+    SidewalkSelectorComponent,
+    TreesComponent,
+    TreeComponent,
+  ],
   imports: [CommonModule, SidewalkRoutingModule, MaterialModule],
 })
 export class SidewalkModule {}
