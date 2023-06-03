02eb682b310d1a8b1ea08a392672810353915132
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-03 11:21:29 -0300

    ✨ Feat: Added redirecting buttons to home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule],
 })
 export class HomeModule {}
