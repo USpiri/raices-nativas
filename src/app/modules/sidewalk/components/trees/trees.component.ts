c9a13a5bec555f7f65d6ceb4fe0e92a6c60603a3
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-07 17:59:04 -0300

    ✨ Feat: Added filter function

--- src/app/modules/sidewalk/components/trees/trees.component.ts
+++ src/app/modules/sidewalk/components/trees/trees.component.ts
@@ -10,14 +10,43 @@ import { TREES } from 'src/assets/data/tree.mock';
 })
 export class TreesComponent {
   trees: Tree[] = TREES;
+  filteredTrees: Tree[] = [];
   modelForm: FormGroup;
+
   private fb = inject(FormBuilder);
+
   constructor() {
     this.modelForm = this.fb.group({
-      max_height: ['Hola'],
-      perennial: [],
-      throns: [],
-      use_case: [],
+      max_height: [null],
+      perennial: [null],
+      thorns: [null],
+      use_case: [null],
     });
+    this.filteredTrees = [...this.trees];
+  }
+
+  filter() {
+    const filterValue = this.modelForm.value;
+    let filteredTrees = [...this.trees];
+
+    Object.entries(filterValue).forEach(([clave, valor]) => {
+      if (valor !== null) {
+        if (typeof valor === 'string') {
+          filteredTrees = filteredTrees.filter((tree) => {
+            const treeValue = String(tree[clave as keyof Tree]).toLowerCase();
+            const filterValue = valor.toLowerCase();
+            return treeValue.includes(filterValue);
+          });
+        } else {
+          filteredTrees = filteredTrees.filter(
+            (tree) => tree[clave as keyof Tree] === valor
+          );
+        }
+      }
+    });
+
+    this.filteredTrees = filteredTrees;
+    console.log('filter', filterValue);
+    console.log('result', this.filteredTrees);
   }
 }
