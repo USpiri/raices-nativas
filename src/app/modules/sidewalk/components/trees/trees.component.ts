f883ba7c19af439e1cc0fa8f8b430f16705a922c
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-07 18:43:57 -0300

    🐛 Fix: Filter does not takes range values of max_height

--- src/app/modules/sidewalk/components/trees/trees.component.ts
+++ src/app/modules/sidewalk/components/trees/trees.component.ts
@@ -31,7 +31,19 @@ export class TreesComponent {
 
     Object.entries(filterValue).forEach(([clave, valor]) => {
       if (valor !== '' && valor !== null) {
-        if (typeof valor === 'string') {
+        if (clave === 'max_height') {
+          filteredTrees = filteredTrees.filter((tree) => {
+            const treeMaxHeight = tree.max_height;
+            if (valor === '4') {
+              return treeMaxHeight <= 4;
+            } else if (valor === '9') {
+              return treeMaxHeight > 4 && treeMaxHeight <= 9;
+            } else if (valor === '+9') {
+              return treeMaxHeight > 9;
+            }
+            return true; // Para otros valores que no sean los grupos específicos
+          });
+        } else if (typeof valor === 'string') {
           filteredTrees = filteredTrees.filter((tree) => {
             const treeValue = String(tree[clave as keyof Tree]).toLowerCase();
             const filterValue = valor.toLowerCase();
